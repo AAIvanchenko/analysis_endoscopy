42a6febaf838cdb8940bd56d1fac0b36c5659fa1
Anastasia Ivanchenko <89zazaza89@gmail.com>
2022-10-24 23:43:49 +0700

    add model

--- src/model.py
+++ src/model.py
@@ -1,11 +1,10 @@
 from torch import nn
 import torch.nn.functional as F
-import torch
 
 
-class Model1(nn.Module):
+class Model(nn.Module):
     def __init__(self):
-        super(Model1, self).__init__()
+        super(Model, self).__init__()
         # 1 input image channel, 6 output channels, 3x3 square conv kernel
         self.conv1 = nn.Conv2d(3, 8, 3)
         self.bn1 = nn.BatchNorm2d(8)
@@ -24,25 +23,6 @@ class Model1(nn.Module):
         x = self.bn3(F.max_pool2d(F.relu(self.conv3(x)), 2))
         x = x.view(x.shape[0], -1)
         x = self.bn4(F.relu(self.fc1(x)))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-    
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 2)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
