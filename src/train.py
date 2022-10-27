47e37da571c642e88192dc2fc481cdc28dda01f2
Anastasia Ivanchenko <89zazaza89@gmail.com>
2022-10-28 02:10:21 +0700

    Trained model 4 layers data augment

--- src/train.py
+++ src/train.py
@@ -15,18 +15,26 @@ ROOT = FILE.parents[1] # program ROOT
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(59536, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 2)
+        # 1 input image channel, 6 output channels, 3x3 square conv kernel
+        self.conv1 = nn.Conv2d(3, 8, 3)
+        self.bn1 = nn.BatchNorm2d(8)
+        self.conv2 = nn.Conv2d(8, 16, 3)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 32, 3)
+        self.conv4 = nn.Conv2d(32, 64, 5, stride = 2, padding = 1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.fc1 = nn.Linear(3136, 256)
+        self.bn4 = nn.BatchNorm1d(256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, 2)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
+        x = self.bn1(F.max_pool2d(F.relu(self.conv1(x)), 2))
+        x = self.bn2(F.max_pool2d(F.relu(self.conv2(x)), 2))
+        x = F.max_pool2d(F.relu(self.conv3(x)), 2)
+        x = self.bn3(F.max_pool2d(F.relu(self.conv4(x)), 2))
+        x = x.view(x.shape[0], -1)
+        x = self.bn4(F.relu(self.fc1(x)))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
